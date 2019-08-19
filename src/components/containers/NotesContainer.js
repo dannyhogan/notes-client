ab4d1adceff98239df970791e904b7f09d588194
Dan Hogan <hogan.daniel@hotmail.com>
2019-08-19 13:16:20 -0700

    Create NotesContainer component

--- src/components/containers/NotesContainer.js
+++ src/components/containers/NotesContainer.js
@@ -24,6 +24,7 @@ class NotesContainer extends Component {
   componentDidMount() {
     getNotes()
       .then(notes => {
+        console.log(notes);
         this.setState({ notes });
       });
   }
